ccaa99528ea5263f8fd4c5ac762c64f9707ff967
new-village <kazuki.niimura@gmail.com>
2024-08-12 08:33:31 +0000

    support complete static page

--- src/components/CorporatePage.tsx
+++ src/components/CorporatePage.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
 import Papa from 'papaparse';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { Card, Button, Table, Breadcrumb } from 'react-bootstrap';
 
 function CorporatePage() {
   const [data, setData] = useState<string[][]>([]);
-  const [filteredData, setFilteredData] = useState<string[][]>([]);
+  const [filtered, setFilteredData] = useState<string[][]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const navigate = useNavigate();
+  const [selectedCorporate, setSelectedCorporate] = useState<string[][] | null>(null);
   const location = useLocation();
-  const rowsPerPage = 15;
+  const rowsPerPage = 10;
 
   useEffect(() => {
     fetch('data.csv')
@@ -39,50 +39,54 @@ function CorporatePage() {
     setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
   };
 
+  const handleRowClick = (row: string[]) => {
+    setSelectedCorporate([row]);
+  };
+
   const startIndex = (currentPage - 1) * rowsPerPage;
   const paginatedData = data.slice(startIndex, startIndex + rowsPerPage);
 
   return (
     <div>
-      {filteredData.length > 0 ? (
-        filteredData.map((row, rowIndex) => (
+      {selectedCorporate ? (
+        selectedCorporate.map((row, rowIndex) => (
           <div key={rowIndex}>
-            <Breadcrumb>
-                <Breadcrumb.Item onClick={() => navigate(-1)}>法人リスト</Breadcrumb.Item>
-                <Breadcrumb.Item active>法人詳細</Breadcrumb.Item>
-            </Breadcrumb>
-            <h1>{row[6]}</h1>
-            <hr/>
-            <div className='secondRow'>
-            <Card className='card-margin-right'>
-              <Card.Body>
-                <Card.Title>法人概要</Card.Title>
-                <Card.Text as="div">
-                    <ul>
-                        <li><strong>法人番号</strong>: {row[1]}</li>
-                        <li><strong>法人名（カナ）</strong>: {row[28]}</li>
-                        <li><strong>法人名（英）</strong>: {row[24]}</li>
-                        <li><strong>登記日</strong>: {row[5]}</li>
-                        <li><strong>最終更新日</strong>: {row[4]}</li>
-                    </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <Breadcrumb>
+              <Breadcrumb.Item href="./corporate">法人リスト</Breadcrumb.Item>
+              <Breadcrumb.Item active>法人詳細</Breadcrumb.Item>
+          </Breadcrumb>
+          <h1>{row[6]}</h1>
+          <hr/>
+          <div className='secondRow'>
+          <Card className='card-margin-right'>
+            <Card.Body>
+              <Card.Title>法人概要</Card.Title>
+              <Card.Text as="div">
+                  <ul>
+                      <li><strong>法人番号</strong>: {row[1]}</li>
+                      <li><strong>法人名（カナ）</strong>: {row[28]}</li>
+                      <li><strong>法人名（英）</strong>: {row[24]}</li>
+                      <li><strong>登記日</strong>: {row[5]}</li>
+                      <li><strong>最終更新日</strong>: {row[4]}</li>
+                  </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
 
-            <Card className='card-margin-left'>
-              <Card.Body>
-                <Card.Title>所在地情報</Card.Title>
-                <Card.Text as="div">
-                    <ul>
-                        <li><strong>郵便番号</strong>: {formatPostalCode(row[15])}</li>
-                        <li><strong>住所</strong>: {formatAddress(row[9], row[10], row[11])}</li>
-                        <li><strong>住所（アルファベット）</strong>: {formatAddress(row[26], ", ", row[25])}</li>
-                    </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-            </div>
+          <Card className='card-margin-left'>
+            <Card.Body>
+              <Card.Title>所在地情報</Card.Title>
+              <Card.Text as="div">
+                  <ul>
+                      <li><strong>郵便番号</strong>: {formatPostalCode(row[15])}</li>
+                      <li><strong>住所</strong>: {formatAddress(row[9], row[10], row[11])}</li>
+                      <li><strong>住所（アルファベット）</strong>: {formatAddress(row[26], ", ", row[25])}</li>
+                  </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
           </div>
+      </div>
         ))
       ) : (
         <div>
@@ -101,8 +105,8 @@ function CorporatePage() {
             </thead>
             <tbody>
               {paginatedData.map((row, rowIndex) => (
-                <tr key={rowIndex}>
-                  <td><a href={`./corporate?id=${row[1]}`}>{row[1]}</a></td>
+                <tr key={rowIndex} onClick={() => handleRowClick(row)}>
+                  <td>{row[1]}</td>
                   <td>{row[6]}</td>
                   <td>{row[15]}</td>
                   <td>{row[9]}</td>
